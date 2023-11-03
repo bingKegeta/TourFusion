7795c5d93501e79f28750cefaad0adbda5b31b26
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2023-11-02 23:13:30 -0400

    Replaced class for interface. Changed var to consts, now the event is properly handled by react

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,92 @@
-import { Viewer, ScreenSpaceEventHandler, Math as CMath, Cartesian3, ScreenSpaceEventType } from "cesium";
+import { Viewer, ScreenSpaceEventHandler, Math as CMath, Cartesian3, Cartesian2, ScreenSpaceEventType } from "cesium";
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
-
-// const fetch = require("node-fetch")
-
+import { CCPosition } from "./common/types";
 
 function App() {
 
-  class CCPosition {
-    public latitude: number = 0.0;
-    public longitude: number = 0.0;
-    public height: number = -100.0;
-
-    public constructor(init?:Partial<CCPosition>) {
-      Object.assign(this, init);
-    }
-
-    public reassign(values?: Partial<CCPosition>) {
-      Object.assign(this, values);
-    }
-  }
   
   const [map, setMap] = useState<Viewer|null>(null);
   const divRef = useRef<HTMLDivElement>(null);
-  const [clickedPos, setClickedPos] = useState<CCPosition>(new CCPosition());
+
+  const [clickedPos, setClickedPos] = useState<CCPosition | null>(null);
 
   useEffect(() => {
     if (divRef.current) {
-      setMap(new Viewer(divRef.current));
-      return () => map?.destroy();
+      const viewerInstance = new Viewer(divRef.current);
+      setMap(viewerInstance);
+
+      return () => viewerInstance?.destroy();
     }
   }, []);
 
-  // const queryGraphQL = () => {
-  //   fetch('https://localhost:5000/api', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json',
-  //       'X-Api-Service-Key': '123456789077',
-  //     },
-  //     body: JSON.stringify({query:
-  //      `query{
-  //         addUser(
-  //           user_id: $user_id
-  //           name: $name
-  //           latitude: $latitude
-  //           longitude: $longitude
-  //           )
-  //       }`,
-  //     variables: { "user_id": "653bfedf1e7c5a2367365f16", "name": "PLACE Y", "latitude": , "longitude": }})
-  //   })
-  //     .then(r => r.json())
-  //     .then(data => console.log('data returned:', data));
-  // }
-
   // Get latitude and longitude
   // TODO: Fix this: It executes every click again every time a new click is encountered
+  // Resolution: Now the event is handled by react on a double click, as opposed to cesium's event handler
+
+
   // TODO: Make the name of the place more modular using cesium's reverse geocoding or something
+  
+  
   //! The first click on refresh or just moving the globe is (0,0) coords - wrong
+  // Resolution: The previous version was handling the event on cesium's event handler.
+  // While that worked, the async nature of the useState hook was causing the initial coordiante to be the default one.
+  // Now, we are passing the click event which contains the part of the screen that was clicked.
+  // Then, we are using the camera to pick the ellipsoid at that point and get the coordinates.
+  
   //* Suggestion: Add location only on double click and prompt again to be sure if user wants to add that location
   //? Clean the code to make it more modular?
-	const getPositionOnClick = async () => {
+	const getPositionOnClick = (clicked : any) => {
+
+
     if (map === null)
       return;
-    var viewer = map;
-    var scene = viewer.scene;
-    var ellipsoid = viewer.scene.globe.ellipsoid;
-    var longitude = 0.0;
-    var latitude = 0.0;
-    var height = 0.0;
-    var cartesian: Cartesian3 | undefined = undefined;
-    var handler = new ScreenSpaceEventHandler(scene.canvas);
-    handler.setInputAction(function(movement: any) {
-      cartesian = viewer.camera.pickEllipsoid(movement.position, ellipsoid);
-      if (cartesian) {
-          var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-          longitude = CMath.toDegrees(cartographic.longitude);
-          latitude = CMath.toDegrees(cartographic.latitude);
-          height = Math.ceil(viewer.camera.positionCartographic.height);
-          setClickedPos(new CCPosition({latitude, longitude, height}));
-      }
-    }, ScreenSpaceEventType.LEFT_CLICK);
-    handler.setInputAction(function(wheelment: any) {
-        latitude = clickedPos.latitude;
-        longitude = clickedPos.longitude;
-        height = Math.ceil(viewer.camera.positionCartographic.height);
-        setClickedPos(new CCPosition({latitude, longitude, height}));
-      }, ScreenSpaceEventType.WHEEL);
+    const viewer = map;  
+    const ellipsoid = viewer.scene.globe.ellipsoid;
+  
+    let cartesian: Cartesian3 | undefined = undefined;
+    
+    cartesian = viewer.camera.pickEllipsoid(new Cartesian2(clicked.clientX, clicked.clientY), ellipsoid);
+    if (cartesian) {
+        let cartographic = ellipsoid.cartesianToCartographic(cartesian);
+        const longitude = CMath.toDegrees(cartographic.longitude);
+        const latitude = CMath.toDegrees(cartographic.latitude);
+        const height = Math.ceil(viewer.camera.positionCartographic.height);
+        
+        console.log("Latitude: " + latitude);
+        console.log("Longitude: " + longitude);
+        console.log("Height: " + height);
 
-    console.log(clickedPos.latitude);
-    console.log(clickedPos.longitude);
-    console.log(clickedPos.height);
+        setClickedPos({latitude, longitude, height});
+    }
+      {/*
+      //! Only use for testing, the error on unable to add due to same location should be handled too
+      // TODO: Convert to separate function/file for all the possible queries
+      const endpoint = 'http://localhost:5000/api';
+      const name = 'Place A';
+      const addLocationQuery = JSON.stringify({
+        query: `mutation {
+          addLocation(user_id: "6537f36acdd7568258da16d5", name: "${name}", latitude: ${clickedPos.latitude}, longitude: ${clickedPos.longitude})
+        }`
+      });
 
-    //! Only use for testing, the error on unable to add due to same location should be handled too
-    // TODO: Convert to separate function/file for all the possible queries
-    const endpoint = 'http://localhost:5000/api';
-    const name = 'Place A';
-    const addLocationQuery = JSON.stringify({
-      query: `mutation {
-        addLocation(user_id: "6537f36acdd7568258da16d5", name: "${name}", latitude: ${clickedPos.latitude}, longitude: ${clickedPos.longitude})
-      }`
-    });
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: addLocationQuery,
+      });
+      const data = await response.json();
 
-    const response = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: addLocationQuery,
-    });
-    const data = await response.json();
-
-    console.log(data);
+    console.log(data);*/}
+    
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="cesium" ref={divRef} onClick={() => {getPositionOnClick()}}/>
+        <div className="cesium" ref={divRef} onDoubleClick = {getPositionOnClick}/>
         <div className="right-container">
           {}
         </div>
