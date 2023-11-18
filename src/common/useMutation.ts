38942f023fd28b416c038971c24a9ebb102cb6ab
Soham Ganguly <sohamg24@gmail.com>
2023-11-18 02:34:40 -0500

    make a useMutation hook

---
+++ src/common/useMutation.ts
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface MutationHookResult<T> {
+  executeMutation: (mutation: string, variables?: any) => Promise<T>;
+  loading: boolean;
+  error: Error | null;
+}
+
+const useMutation = <T>(url: string): MutationHookResult<T> => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const executeMutation = async (
+    mutation: string,
+    variables?: any
+  ): Promise<T> => {
+    setLoading(true);
+
+    try {
+      const response = await axios.post(url, { query: mutation, variables });
+      return response.data.data;
+    } catch (err: any) {
+      console.error("Error executing mutation:", err);
+      setError(err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { executeMutation, loading, error };
+};
+
+export default useMutation;
