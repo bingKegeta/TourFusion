dabcf876d886683cf937b23986009eca14746bf5
Soham Ganguly <sohamg24@gmail.com>
2023-11-18 20:17:23 -0500

    cooking some mid food

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,27 +1,28 @@
 export interface CCPosition {
-    latitude: number;
-    longitude: number;
-    height: number;
+  latitude: number;
+  longitude: number;
+  height: number;
 }
 
 export interface CCLocation {
-    street: string | undefined;
-    city: string | undefined;
-    country: string | undefined;
-    address: string | undefined;
-    postal: string | undefined;
+  display: string;
+  street: string | undefined;
+  city: string | undefined;
+  country: string | undefined;
+  address: string | undefined;
+  postal: string | undefined;
 }
 
 export interface TourFusionLocation {
-    name: CCLocation;
-    location: CCPosition;
-    elevation: number;
-    averageTemperature: number;
-    trewarthaClassification: string;
-    climateZone: string;
+  name: CCLocation;
+  location: CCPosition;
+  elevation: number;
+  averageTemperature: number;
+  trewarthaClassification: string;
+  climateZone: string;
 }
 
 export interface changeType {
-    onUpdateClickedPos: (value: CCPosition) => void;
-    onUpdateClickedLoc: (value: CCLocation) => void;
+  onUpdateClickedPos: (value: CCPosition) => void;
+  onUpdateClickedLoc: (value: CCLocation) => void;
 }
