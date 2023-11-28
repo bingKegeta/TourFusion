9ebd88e501af3ad94a1c32908f932c8a5879d9d7
Alejandro Palmar <52765355+alejoseed@users.noreply.github.com>
2023-11-27 23:34:38 -0500

    Fixed some CSS

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const LoadingPage: React.FC = () => {
   return (
     <div className="flex flex-col h-full w-full z-50 opacity-80 justify-items-center
-              absolute items-center justify-center bg-gray-900 text-white">
+              absolute items-center justify-center bg-gray-900 text-white ">
       {/* <div className="animate-spin rounded-full border-t-4 border-white h-16 w-16 mb-4"></div>
       <h2 className="text-lg">Loading...</h2> */}
-      <img src="/loading.gif" alt="loading..." />
+      <img src="/loading.gif" alt="loading..."  className="shadow-lg rounded-lg border-4 shadow-red-300"/>
       <h2 className="text-lg">C'mon snowy! Let's head to the dashboard...</h2>
     </div>
   );
